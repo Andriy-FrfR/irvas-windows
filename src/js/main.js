a78aacf356cef9f576e3072dfdaefdcd81a5839c
unknown <andriy.lukashewwich@gmail.com>
2021-06-02 20:04:34 +0300

    moved slider configuration to modules folder

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
+import {configurateSlick} from './modules/slick-slider-config.js';
 import {tabs} from './modules/tabs.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  configurateSlick();
+  
   tabs();
 });
---
+++ src/js/modules/slick-slider-config.js
@@ -0,0 +1,70 @@
+export const configurateSlick = () => {
+  $('.glazing-slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    canselable: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 529,
+        settings: {
+          slidesToShow: 1,
+          arrows: true
+        }
+      }
+    ]
+  });
+
+  $('.order-finishing-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    canselable: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          arrows: true
+        }
+      }
+    ]
+  });
+};
